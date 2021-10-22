d9af80f2ca182754184eff109ec4a75f108a467b
trinhdvt <trinhvideo123@gmail.com>
2021-10-22 15:25:25 +0700

    add break time after download to prevent socket error

--- src/utils/google_utils.py
+++ src/utils/google_utils.py
@@ -1,6 +1,9 @@
 import gdown
+import time
 
 
 def download_model(drive_id, output_path):
     url = f"https://drive.google.com/uc?id={drive_id}"
     gdown.download(url, output_path)
+    print("Sleeping ...")
+    time.sleep(1)
