c001c812c19c9e080f8bb979cf33af958a93f973
trinhdvt <trinhvideo123@gmail.com>
2021-10-28 22:52:26 +0700

    modified predict function
    several bounding box of one class will be concatenated to one box only

--- src/card_recognition/detector.py
+++ src/card_recognition/detector.py
@@ -1,86 +1,95 @@
 import torch
-from PIL import Image
 
 
 class Detector:
-    def __init__(self, weight_path, size=320, conf=0.5, iou=0.5):
+    def __init__(self, weight_path, size=416, conf=0.4, iou=0.5):
         self.model = torch.hub.load("ultralytics/yolov5", 'custom', path=weight_path)
         self.model.conf = conf
         self.model.iou = iou
         self.size = size
         self.class_names = self.model.model.names
 
-    def predict(self, imgs):
-        """
-        Detect information from image
-
-        :param imgs: PIL Image
-        :return: Detected result
-        """
-
-        #
-        results = self.model(imgs, size=self.size)
-
-        #
-        final_results = [[
-            [
-                {
-                    "class": int(pred[5]),
-                    "class_name": self.class_names[int(pred[5])],
-                    "normalized_box": pred[:4].tolist(),
-                    "confidence": float(pred[4])
-                }
-            ] for pred in result
-        ] for result in results.xyxyn]
-
-        #
-        return final_results
-
-    def detect_crop(self, image, save_crop=False, verbose=False):
+    def detect_crop(self, image, save_file=False, verbose=False):
         """
         Detect and crop information from image then save to a dict
 
-        :param save_crop: True to save cropped images
+        :param save_file: Directory to save detect image
         :param image: PIL Image
         :param verbose: True to show processing time
         :return: Dictionary with key is the class's name and value is the cropped image
         """
 
-        #
+        # inference
         results = self.model(image, size=self.size)
 
-        #
+        # print process step
         if verbose:
             results.print()
+        if save_file:
+            results.save()
 
-        # print(results.pandas().xyxy[0])
+        # get bounding box from detected results
+        bounding_box = self.get_bounding_box(results, image)
+
+        # crop image for each class
+        return self.crop(image, bounding_box)
+
+    def get_bounding_box(self, detect_rs, img):
+        """
+        Get bounding box for each class from detected results
+
+        :param detect_rs: Detect results from YOLOV5 model
+        :param img: Original image
+        :return: An dictionary: Key [label] -> Bounding_box [x_min, y_min, x_max, y_max] or None
+        """
+
+        detect_rs = detect_rs.xyxyn[0].tolist()
+        """
+             xmin    ymin    xmax   ymax  confidence  class
+        0  749.50   43.50  1148.0  704.5    0.874023      0
+        1  433.50  433.50   517.5  714.5    0.687988      1
+        """
 
         #
-        cropped = results.crop(save=save_crop)
+        width, height = img.size
+        bounding_box = {}
 
-        # post-processing
-        detect_rs = {k: [] for k in self.class_names}
-
-        for rs in cropped:
-            # label format: <label_name> <probability_value>
-            label = rs['label'].split(' ')[0]
-
-            # box format: list of tensor [x_min, y_min, x_max, y_max]
-            box = rs['box']
-            box = list(map(lambda x: x.item(), box))  # list[tensor] -> list[float]
-            x_min = box[0]  # get x_min to sort detected images in same class
-
-            # image format: numpy array
-            im = rs['im'][..., ::-1]  # reshape to HxWxC
+        # get bounding box and concat them for each class
+        for row in detect_rs:
+            #
+            label = self.class_names[int(row[5])]
+            if label not in bounding_box.keys():
+                bounding_box[label] = [width + 100, height + 100, 0, 0]
 
             #
-            detect_rs[label].append((x_min, im))
+            bbox = row[:4]
+            current_row = bounding_box[label]
+            current_row[0] = min(int(width * bbox[0]), current_row[0])
+            current_row[1] = min(int(height * bbox[1]), current_row[1])
+            current_row[2] = max(int(width * bbox[2]), current_row[2])
+            current_row[3] = max(int(height * bbox[3]), current_row[3])
 
-        # one class may have several results, sort them by x_min
-        # results format: key -> list[PIL image]
-        for k, v in detect_rs.items():
-            detect_rs[k] = sorted(v, key=lambda x: x[0])
-            detect_rs[k] = list(map(lambda x: Image.fromarray(x[1], 'RGB'), detect_rs[k]))
+        return bounding_box
 
-        #
-        return detect_rs
+    def crop(self, img, bounding_box):
+        """
+        Crop image for each class
+
+        :param img: Original image
+        :param bounding_box: Bounding box of each class
+        :return: A dictionary: Key [label] -> Value [PIL Image] or None
+        """
+
+        crop_rs = {
+            label: None for label in self.class_names
+        }
+        padding = [-5, -2, 5, 2]
+        for label, bbox in bounding_box.items():
+            if len(bbox) != 4:
+                continue
+
+            # adding some padding
+            bbox = list(map(lambda x, y: x + y, padding, bbox))
+            crop_rs[label] = img.crop(bbox)
+
+        return crop_rs
