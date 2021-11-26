f332e91eecaa1239c36d85a66b458ec5f2d8fc49
trinhdvt <trinhvideo123@gmail.com>
2021-11-26 16:09:11 +0700

    update gunicorn's config for FastAPI

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,10 +1,12 @@
 import multiprocessing
+import os
 
-bind = "0.0.0.0:8080"
-# workers = multiprocessing.cpu_count() * 2 + 1
-workers = 1
-threads = multiprocessing.cpu_count() * 2
+bind = f"0.0.0.0:{os.getenv('PORT', '8080')}"
+workers = multiprocessing.cpu_count() * 2 if os.getenv("ENV") == "DEPLOY" else 1
 timeout = 90
-wsgi_app = "server:app"
-loglevel = "debug"
-capture_output = True
+wsgi_app = "fastapi_server:app"
+loglevel = "info"
+capture_output = False
+worker_class = os.getenv("WORKER_CLASS", "uvicorn.workers.UvicornWorker")
+accesslog = "-"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(T)s %(M)s %(D)s %(L)s'
