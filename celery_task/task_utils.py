41976996aaac600ce6a7b4446787cda8c9256d2e
trinhdvt <trinhvideo123@gmail.com>
2021-12-09 15:58:08 +0700

    refactor code, add function to get backup image from s3

--- celery_task/task_utils.py
+++ celery_task/task_utils.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 from base64 import b64decode, b64encode
 from io import BytesIO
@@ -30,18 +31,18 @@ def base64_to_pil(base64_str: str) -> Image.Image:
 
 async def submit_results(extracted_rs: Dict[str, Optional[str]]):
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    default_url = "http://illusion.codes:9999/api/records"
+    default_url = "http://illusion.codes:9999"
     submit_url = os.getenv("BE_SERVER", default=default_url)
 
     data = {
         'examCode': extracted_rs['examCode'],
-        "status": extracted_rs['match'],
-        'studentId': extracted_rs['id'],
-        'studentName': extracted_rs['name'],
-        'className': extracted_rs['class'],
-        "facultyName": extracted_rs['faculty'],
-        'year': extracted_rs['year'],
-        'birthday': extracted_rs['birth_day'],
+        "status": extracted_rs.get('match', 0),
+        'studentId': extracted_rs.get('id', None),
+        'studentName': extracted_rs.get('name', None),
+        'className': extracted_rs.get('class', None),
+        "facultyName": extracted_rs.get('faculty', None),
+        'year': extracted_rs.get('year', None),
+        'birthday': extracted_rs.get('birth_day', None),
         'ipAddress': extracted_rs['ipAddress'],
         'userAgent': extracted_rs['userAgent'],
         'faceImg': extracted_rs['faceImg'],
@@ -56,14 +57,29 @@ async def submit_results(extracted_rs: Dict[str, Optional[str]]):
         logger.debug(f"Submit failed: {str(e)}")
 
 
-async def save_log_img(img_list: List[np.ndarray], filename: List[str], save_dir: str):
+async def save_log_img(img_list: List[np.ndarray],
+                       filename: List[str],
+                       save_dir: str):
     if len(img_list) != len(filename):
         logger.error(f"img_list and filename length not equal: {len(img_list)} vs {len(filename)}")
         return
 
-    for img, path in zip(img_list, filename):
-        await save_img(img, save_dir + path)
+    async def save_img(img_: np.ndarray,
+                       out_path: str):
+        pil_image = cv2_to_pil(img_)
+        pil_image.thumbnail((600, 600), Image.ANTIALIAS)
+        pil_image.save(out_path, quality=85)
+
+    task_list = [save_img(img, save_dir + path) for img, path in zip(img_list, filename)]
+    await asyncio.gather(*task_list)
 
 
-async def save_img(img: np.ndarray, out_path: str):
-    cv2_to_pil(img).save(out_path, quality=85)
+async def get_backup_image(student_id: str) -> Optional[Image.Image]:
+    base_url = os.getenv("BACKUP_IMG_URL", default="https://pbl6-sv-img.s3.ap-southeast-1.amazonaws.com")
+    img_url = f"{base_url}/{student_id}.jpg"
+    #
+    response = requests.get(img_url, stream=True)
+    if response.status_code == 200:
+        return Image.open(response.raw).convert('RGB')
+    else:
+        return None
